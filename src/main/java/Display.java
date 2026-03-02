81955a8f6dcfc8cd56507835ce67aa7f2f060e25
venujan nagendirakumar <venujan25@outlook.com>
2026-03-02 15:00:26 +0100

    implemented chooseProduct method

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,6 +1,9 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Display {
+
+    Scanner scanner = new Scanner(System.in);
 
     public void showManu(List<Product> products) {
         for (int i = 0; i < products.size(); i++) {
@@ -11,5 +14,11 @@ public class Display {
                     +product.getPrice() + "kr");
         }
 
+    }
+
+    public int chooseProduct(Scanner scanner) {
+        System.out.println("Välj en produkt");
+        int choice = scanner.nextInt();
+        return choice;
     }
 }
