6481e0235807a12f5106b17b9a2f6799268fef09
venujan nagendirakumar <venujan25@outlook.com>
2026-03-02 14:52:34 +0100

    implemented showManu method in Display cllass

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -1,2 +1,15 @@
+import java.util.List;
+
 public class Display {
+
+    public void showManu(List<Product> products) {
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            System.out.println((i+1) + "."
+                    +product.getName()+
+                    "-"
+                    +product.getPrice() + "kr");
+        }
+
+    }
 }
