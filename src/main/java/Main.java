f8983c2b5c8a50a3dbcd85c0deef8c2635511968
venujan nagendirakumar <venujan25@outlook.com>
2026-03-02 15:10:43 +0100

    created objects of classes in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        CheckOut checkOut = new CheckOut();
+        Display display = new Display();
+        Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+    }
 }
